2c70d0d0d6982360c11fb5d075d530a303863894
Ramprasad <ramprasad.s.m28@gmail.com>
2026-03-14 23:33:22 +0530

    Day 2

---
+++ backend/static/js/dashboard.js
@@ -0,0 +1,55 @@
+console.log("dashboard.js loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch('/api/dashboard/kpi/')
+    .then(response => response.json())
+    .then(data => {
+
+        document.getElementById("revenue").innerText = data.total_revenue;
+        document.getElementById("orders").innerText = data.total_orders;
+        document.getElementById("avg").innerText = data.average_order_value;
+
+    });
+
+
+    fetch('/api/dashboard/product-revenue/')
+    .then(response => response.json())
+    .then(data => {
+
+        const labels = data.map(item => item.product);
+        const values = data.map(item => item.total_revenue);
+
+        new Chart(document.getElementById('productChart'), {
+            type:'bar',
+            data:{
+                labels:labels,
+                datasets:[{
+                    label:'Revenue',
+                    data:values
+                }]
+            }
+        });
+
+    });
+
+
+    fetch('/api/dashboard/order-status/')
+    .then(response => response.json())
+    .then(data => {
+
+        const labels = data.map(item => item.status);
+        const values = data.map(item => item.count);
+
+        new Chart(document.getElementById('statusChart'), {
+            type:'pie',
+            data:{
+                labels:labels,
+                datasets:[{
+                    data:values
+                }]
+            }
+        });
+
+    });
+
+});
